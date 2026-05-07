2bf4261234ddd0f0fbda2cfd67fefa6b38ed80df
Keerthu7 <keerthanasubbu5@gmail.com>
2026-05-07 16:37:46 +0530

    images changes

--- components/home/work-section.tsx
+++ components/home/work-section.tsx
@@ -64,7 +64,7 @@ export default function WorkSection() {
             {
               title: "Team3 Associates",
               category: "Custom Development",
-              image: "/team3_logo.png",
+              image: "/team3_works.png",
               description: "Interior Design and Project Execution",
             },
             {
